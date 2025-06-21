eac64a14da71d1f27b30fc8bb4f2f803b2fe9cf5
impossible589 <91239042+impossible589@users.noreply.github.com>
2025-06-21 22:18:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__, static_folder='static')
 @app.route('/')
 def serve_index():
     return send_from_directory(app.static_folder, 'index.html')
-
+@app.route('/loaderio-6eb9bf95da20f05ae75ff6a724761fb3.txt')
+def servetest():
+    return send_from_directory(app.static_folder,'loaderio-6eb9bf95da20f05ae75ff6a724761fb3.txt')
 @app.route('/api/hello')
 def hello():
     return {"message": "Hello from Python backend"}
